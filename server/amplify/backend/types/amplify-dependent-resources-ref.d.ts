bdc6bb88e6166ed6351170b9366049c7a7309097
matthewjparkes <78561107+matthewjparkes@users.noreply.github.com>
2022-09-11 18:08:40 +0100

    -m blob

--- server/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ server/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,17 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "objectviewer84973c75": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "ObjectViewerAPI": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
